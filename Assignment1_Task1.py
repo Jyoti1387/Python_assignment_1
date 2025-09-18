e6e39f10572cce59bde61f9d9ba3fb86b2290e5f
Jyoti1387 <62137916+Jyoti1387@users.noreply.github.com>
2025-09-18 16:02:03 +0530

    Add files via upload

---
+++ Assignment1_Task1.py
@@ -0,0 +1,16 @@
+a = int(input('Enter the first number : '))
+b = int (input('Enter the Second number : '))
+
+# Addition
+add = a+b
+# Subtraction
+sub = a-b
+# Multiplication
+mul = a*b
+# Division
+div =  a/b
+
+print ("Addition : ", add)
+print ("Subtraction : ", sub)
+print ("Multiplication : ", mul)
+print ("Division : ", div)
---
+++ Assignment1_Task2.py
@@ -0,0 +1,4 @@
+fname = input('Enter your First name : ')
+lname = input('Enter your last name : ')
+full_name = fname+'\t'+lname
+print('Hello , '+full_name+' ! Welcome to the Python Program')
